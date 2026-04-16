80526e133c3cf6bfb7a337972725516b5255dd9b
Arnaud <arnaud.malapert@univ-cotedazur.fr>
2026-04-16 16:31:55 +0200

    update gulp file and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,114 @@
-const $ = require('gulp');
-const $changed = require('gulp-changed');
-const $htmlmin = require('gulp-htmlmin');
-const $plumber = require('gulp-plumber');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
 
-const del = require('del');
-const server = require('browser-sync').create();
+const { deleteAsync } = require('del');
+const browserSync = require('browser-sync').create();
 
-$.task('clean', clean);
-$.task('publish', publish);
-$.task('minimize', pages);
-$.task('build', $.series('clean', 'publish', 'minimize'));
-$.task('rebuild', $.series('publish', 'minimize'));
-$.task('default', $.series('build', $.parallel(serve, watch)));
+const through2 = require('through2');
+const { minify } = require('html-minifier-terser');
+const { spawn } = require('child_process');
+
+/* =========================
+   TASKS (Gulp 4 style)
+========================= */
+
+gulp.task('clean', clean);
+gulp.task('publish', publish);
+gulp.task('minimize', pages);
+
+gulp.task('build', gulp.series('clean', 'publish', 'minimize'));
+gulp.task('rebuild', gulp.series('publish', 'minimize'));
+
+gulp.task('default', gulp.series('build', gulp.parallel(serve, watch)));
+
+/* =========================
+   CLEAN
+========================= */
 
 function clean() {
-    // TODO Find home dir : https://stackoverflow.com/questions/34713622/accessing-home-directory-with-base-in-gulp
-    return del(['build', '/home/nono/.org-timestamps/org-r-notes.cache', '/home/nono/.org-timestamps/org-r-static.cache'], {force:true});
+    return deleteAsync([
+        'build',
+        '/home/nono/.org-timestamps/org-r-notes.cache',
+        '/home/nono/.org-timestamps/org-r-static.cache'
+    ], { force: true });
 }
 
-var spawn = require('child_process').spawn;
+/* =========================
+   EMACS PUBLISH
+========================= */
 
 function publish(cb) {
-    //var cmd = spawn('cmd', ['arg1', 'agr2'], {stdio: 'inherit'});
-    // var cmd = spawn('ls', [], {stdio: 'inherit'});
     // Emacs Workaround : conflict between whitespace-style and org-publish.
-    var cmd = spawn('emacs', ['--eval', '(prog1 (setq whitespace-style \'(face))(org-publish-project "org-r")(kill-emacs))'], {stdio: 'inherit'});
-    cmd.on('close', function (code) {
+  const cmd = spawn(
+    'emacs',
+    [
+      '--eval',
+      `(prog1
+         (setq whitespace-style '(face))
+         (org-publish-project "org-r")
+         (kill-emacs))`
+    ],
+    { stdio: 'inherit' }
+  );
+
+  cmd.on('close', (code) => {
     console.log('emacs org-publish exited with code ' + code);
     cb(code);
-    });
+  });
 }
 
+/* =========================
+   HTML MINIFY STREAM
+========================= */
+
+function htmlMinify() {
+  return through2.obj(function (file, _, cb) {
+    if (file.isBuffer()) {
+      minify(file.contents.toString(), {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeEmptyAttributes: true,
+        minifyJS: true,
+        minifyCSS: true
+      })
+        .then(result => {
+          file.contents = Buffer.from(result);
+          cb(null, file);
+        })
+        .catch(cb);
+    } else {
+      cb(null, file);
+    }
+  });
+}
+
+/* =========================
+   PAGES
+========================= */
+
+function pages() {
+  return gulp.src('build/**/*.html', { base: './' })
+    .pipe(plumber())
+    .pipe(htmlMinify())
+    .pipe(gulp.dest('./'));
+}
+
+/* =========================
+   WATCH + SERVER
+========================= */
+
 function reload(done) {
-  server.reload();
+  browserSync.reload();
   done();
 }
 
 function watch() {
-  $.watch(['build/*.html'], $.series(pages, reload));
+  gulp.watch('build/**/*.html', gulp.series(pages, reload));
 }
 
 function serve(done) {
-  server.init({server: 'build'});
+  browserSync.init({
+    server: 'build'
+  });
   done();
-}
-
-function pages() {
-    // https://github.com/gulpjs/gulp/issues/267
-    return $.src('build/*.html',  {base: './'})
-    .pipe($changed('build'))
-    .pipe($plumber())
-    .pipe($htmlmin({
-      removeComments: true,
-      collapseWhitespace: true,
-      removeEmptyAttributes: true,
-      minifyJS: true,
-      minifyCSS: true}))
-    .pipe($.dest('./'));
 }
